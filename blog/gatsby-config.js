462bf431d706c54c26a17119195acb0ba3d76cfd
Kazuya Seto <jrt1900@gmail.com>
2020-04-14 14:53:48 +0900

    ブログを追加

---
+++ blog/gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `スタプリブログ`,
+    author: `Kazuya Seto`,
+    description: `ゲーム開発に関連するあれやこれや`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/_seto_`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/kazuyaseto`,
+      },
+    ],
+  },
+}
---
+++ blog/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    ゲーム開発の<Styled.a href="https://kazuyaseto.github.io/">スタジオプリーズ</Styled.a>
+    {` `}.
+    <br />
+    コロナウイルスこわい.
+  </Fragment>
+)
